ffba9e6a0f6f9d35c84d59efd7aa7f12fd0552cf
PadraigPowell <padraig.powell@gmail.com>
2017-02-23 20:38:34 +0000

    Update To login and signup

--- app/src/main/java/project/year/afinal/studentadvice/Login.java
+++ app/src/main/java/project/year/afinal/studentadvice/Login.java
@@ -2,6 +2,7 @@ package project.year.afinal.studentadvice;
 
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -13,8 +14,14 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 
 /**
@@ -22,13 +29,14 @@ import com.facebook.appevents.AppEventsLogger;
  */
 public class Login extends Fragment implements View.OnClickListener{
 
-    private Button buttonLogin;
+    private LoginButton buttonLogin;
+    private LoginButton facebookLogin;
     private TextView textViewSignup;
     private EditText editTextEmail;
     private EditText editTextPassword;
-
-    ProgressDialog progressDialog;
-    View view;
+    private CallbackManager callbackManager;
+    private ProgressDialog progressDialog;
+    private View view;
 
     public Login() {
         // Required empty public constructor
@@ -38,20 +46,71 @@ public class Login extends Fragment implements View.OnClickListener{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        //FacebookSdk.sdkInitialize(getApplicationContext());
+        //AppEventsLogger.activateApp(this);
+
+
         progressDialog = new ProgressDialog(getActivity());
-
         view = inflater.inflate(R.layout.fragment_login, container, false);
-
-        buttonLogin = (Button) view.findViewById(R.id.buttonLogin);
+        buttonLogin = (LoginButton) view.findViewById(R.id.buttonLogin);
         textViewSignup = (TextView) view.findViewById(R.id.textViewSignup);
         editTextEmail = (EditText) view.findViewById(R.id.editTextEmail);
         editTextPassword = (EditText) view.findViewById(R.id.editTextPassword);
 
+        facebookLogin = (LoginButton) view.findViewById(R.id.facebookLogin);
+        callbackManager = CallbackManager.Factory.create();
+        facebookLogin.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                //Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                //handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
         buttonLogin.setOnClickListener(this);
         textViewSignup.setOnClickListener(this);
 
         // Inflate the layout for this fragment
         return view;
+    }
+/*
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(FacebookLoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+*/
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode,resultCode,data);
     }
 
     private void loginUser(){
