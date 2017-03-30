0da7357c620015c620b44a03b0d7f691dbfe7d0c
PadraigPowell <padraig.powell@gmail.com>
2017-03-30 22:43:30 +0100

    working on view my posts got the adapter class into the same file and simplified

--- app/src/main/java/project/year/afinal/studentadvice/MyPosts.java
+++ app/src/main/java/project/year/afinal/studentadvice/MyPosts.java
@@ -1,39 +1,40 @@
 package project.year.afinal.studentadvice;
 
+import android.app.ListActivity;
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.Toast;
-import android.widget.Adapter;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
-import com.firebase.client.Query;
 import com.firebase.client.ValueEventListener;
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.HashMap;
+import android.app.ProgressDialog;
 
 
-public class MyPosts extends Fragment {
+public class MyPosts extends ListFragment {
 
     private static final String TAG = "MyPostsFragment";
     private Firebase mRef;
     private FirebaseAuth mAuth;
-    private List<Post> adviceList;
+    private ArrayList<Post> adviceList;
     private ListView myAdvice;
+    private MyAdviceAdapter m_adapter;
+    private ProgressDialog m_ProgressDialog;
+    private Runnable viewAdvice;
 
     public MyPosts() {
         // Required empty public constructor
@@ -44,15 +45,48 @@ public class MyPosts extends Fragment {
                              Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        View view = inflater.inflate(R.layout.fragment_post_advice, container, false);
-        myAdvice = (ListView) view.findViewById(R.id.listViewMyPosts);
+        View view = inflater.inflate(R.layout.fragment_my_posts, container, false);
+        //myAdvice = (ListView) view.findViewById(R.id.android:list);
         ((MainActivity) getActivity()).getSupportActionBar().setTitle("My Posts");
+        adviceList = new ArrayList<Post>();
 
         mRef = ((MainActivity) getActivity()).myFirebaseRef;
         mAuth = ((MainActivity) getActivity()).mAuth;
+
+        this.m_adapter = new MyAdviceAdapter(getContext(), R.layout.my_advice_row, adviceList);
+        setListAdapter(this.m_adapter);
+
+        viewAdvice = new Runnable(){
+            @Override
+            public void run() {
+                getAdvice();
+            }
+        };
+        Thread thread =  new Thread(null, viewAdvice, "MagentoBackground");
+        thread.start();
+        m_ProgressDialog = ProgressDialog.show(getContext(),
+                "Please wait...", "Retrieving data ...", true);
+
+        return view;
+    }
+
+    private Runnable returnRes = new Runnable() {
+
+        @Override
+        public void run() {
+            if (adviceList != null && adviceList.size() > 0) {
+                m_adapter.notifyDataSetChanged();
+                for (int i = 0; i < adviceList.size(); i++)
+                    m_adapter.add(adviceList.get(i));
+            }
+            m_ProgressDialog.dismiss();
+            m_adapter.notifyDataSetChanged();
+        }
+    };
+
+    private void getAdvice(){
         String uid = mAuth.getCurrentUser().getUid();
 
-        adviceList = new ArrayList<Post>();
         mRef.child("advice").orderByChild("uid").equalTo(uid).addListenerForSingleValueEvent(new ValueEventListener() {
             //onDataChange is called every time the name of the User changes in your Firebase Database
             @Override
@@ -72,9 +106,37 @@ public class MyPosts extends Fragment {
                 Toast.makeText(getContext(), "" + firebaseError.getMessage(), Toast.LENGTH_LONG).show();
             }
         });
+        getActivity().runOnUiThread(returnRes);
+    }
 
+    private class MyAdviceAdapter extends ArrayAdapter<Post> {
 
+        List<Post> adviceList;
 
-        return view;
+        public MyAdviceAdapter(Context context, int textViewResourceId, ArrayList<Post> items) {
+            super(context, textViewResourceId, items);
+            this.adviceList = items;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View v = convertView;
+            if (v == null) {
+                LayoutInflater vi = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                v = vi.inflate(R.layout.my_advice_row, null);
+            }
+            Post Advice = adviceList.get(position);
+            if (Advice != null) {
+                TextView title = (TextView) v.findViewById(R.id.title);
+                TextView messPre = (TextView) v.findViewById(R.id.messagePreview);
+                if (title != null) {
+                    title.setText("Name: " + Advice.getTitle());
+                }
+                if (messPre != null) {
+                    messPre.setText("Status: " + Advice.getMassagePreview(20));
+                }
+            }
+            return v;
+        }
     }
 }
